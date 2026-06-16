74955816027ce4b164ffd96179fbcd50e7c16afe
Luan Luís <luanvwittchind@gmail.com>
2026-06-16 14:26:32 -0300

    feat: implement inventory control and automatic stock deduction

--- sales/serializers.py
+++ sales/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from .models import Sale, SaleItem
+from inventory.models import ProductIngredient
 
 class SaleItemSerializer(serializers.ModelSerializer):
     product_name = serializers.CharField(
         source='product.name',
         read_only=True
     )
+
 
     class Meta:
         model = SaleItem
@@ -30,11 +32,11 @@ class SaleSerializer(serializers.ModelSerializer):
             'total',
             'created_at',
             'items',
-        ]        
+        ]
         read_only_fields = ['total']
 
     def create(self, validated_data):
-        items_data = validated_data.pop('items') 
+        items_data = validated_data.pop('items')
 
         sale = Sale.objects.create(**validated_data)
 
@@ -42,7 +44,7 @@ class SaleSerializer(serializers.ModelSerializer):
 
         for item_data in items_data:
             product = item_data['product']
-            quantity = item_data['quantity']  
+            quantity = item_data['quantity']
 
             unit_price = product.price
             subtotal = unit_price * quantity
@@ -53,7 +55,14 @@ class SaleSerializer(serializers.ModelSerializer):
                 quantity=quantity,
                 unit_price=unit_price,
                 subtotal=subtotal
-            )  
+            )
+
+            recipes = ProductIngredient.objects.filter(product=product)
+
+            for recipe in recipes:
+                ingredient = recipe.ingredient
+                ingredient.current_stock -= recipe.quantity * quantity
+                ingredient.save()
 
             total += subtotal
 
